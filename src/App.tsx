af8c71c67ca7d557dbee8159279776fbd4da0fa3
Matheus Viscki <matheusviscki@gmail.com>
2024-05-24 07:31:44 -0300

    feat: create header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './global.css'
+import {Header} from "./components/Header.tsx";
 
 export function App() {
   return (
     <>
+      <Header />
+      <main></main>
       <h1>Todo</h1>
     </>
   )
