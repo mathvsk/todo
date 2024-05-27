963a25a06a63aa8a96dd2a032174dbd75e0e289b
Matheus Viscki <matheusviscki@gmail.com>
2024-05-27 16:45:45 -0300

    feat: Create a todo form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './global.css'
+import styles from './App.module.css'
+
 import {Header} from "./components/Header.tsx";
+import {PlusCircle} from "phosphor-react";
 
 export function App() {
   return (
     <>
       <Header />
-      <main></main>
-      <h1>Todo</h1>
+      <main>
+        <form className={styles.form}>
+          <input type="text" placeholder="Adicione uma nova tarefa"/>
+          <button type="submit">
+            Criar
+            <PlusCircle size={18} />
+          </button>
+        </form>
+      </main>
     </>
   )
 }
